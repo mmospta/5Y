9bc0cb8f5c09e34bd720b26cfc72444e067ab39f
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-06-21 23:41:57 +0700

    initial project

---
+++ 5Y/5Y/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5Y
+//
+//  Created by Phonthep Aungkanukulwit on 21/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
